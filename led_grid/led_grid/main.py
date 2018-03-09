0b6f6cecb4137a41c46514e75d3d76f4e80f3697
cuileann <cuileann@outlook.ie>
2018-03-09 13:43:11 +0000

    imports & class added to main.py

--- led_grid/led_grid/main.py
+++ led_grid/led_grid/main.py
@@ -1,3 +1,37 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import sys
+import re
+import urllib.request
+import time
+
+class LED_Grid():
+    '''Class defined for the LED board'''
+    def __init__ (self,L):
+        self.grid = [[False]*L for _ in range(L)]
+        self.size=L
+
+# this function turns a rectangle of light, topleft LED to btmRight
+    def turn_on(self, x1,y1,x2,y2):
+        if x1 <= x2 and y1<= y2:
+            for i in range(x1,x2+1):
+                for j in range (y1, y2+1):
+                    self.grid[i][j] = True
+
+# turn off selected lights within the grid
+    def turn_off(self, x1,y1,x2,y2):
+        if x1 <= x2 and y1 <= y2:
+            for i in range(x1,x2+1):
+                for j in range (y1, y2+1):
+                    self.grid[i][j] = False
+
+# switch lights within rectangle one to off or vice-versa
+    def switch(self, x1,y1,x2,y2):
+        if x1<=x2 and y1 <=y2:
+            for i in range(x1,x2+1):
+                for j in range(y1,y2+1):
+                    if self.grid[i][j]==True:
+                        self.grid[i][j]=False
+                    else:
+                        self.grid[i][j]=True
