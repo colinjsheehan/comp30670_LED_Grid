9496163795075514a43beb24554e3d655115167c
cuileann <cuileann@outlook.ie>
2018-03-09 14:15:43 +0000

    test read_file()

--- led_grid/tests/test_led_grid.py
+++ led_grid/tests/test_led_grid.py
@@ -8,3 +8,8 @@ sys.path.append('.')
 import pytest
 from click.testing import CliRunner
 from led_grid import main
+
+def test_read_file():
+    ifile = "./data/input_assign3.txt"
+    buffer = main.read_file(ifile)
+    assert buffer == "10\nturn off 2,4 through 6,9"
