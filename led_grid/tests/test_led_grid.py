c386b36b08ba4ee927915756b45d5df59b783307
cuileann <cuileann@outlook.ie>
2018-03-09 14:17:35 +0000

    test arraySize

--- led_grid/tests/test_led_grid.py
+++ led_grid/tests/test_led_grid.py
@@ -13,3 +13,13 @@ def test_read_file():
     ifile = "./data/input_assign3.txt"
     buffer = main.read_file(ifile)
     assert buffer == "10\nturn off 2,4 through 6,9"
+    arraySize=int(buffer.split("\n")[0])
+    for line in buffer.split("\n")[1:]:
+        cmd, x1, y1, x2, y2 = main.get_cmd(line,arraySize)
+
+    assert arraySize == 10
+    assert cmd == 'turn off'
+    assert x1 == 2
+    assert y1 == 4
+    assert x2 == 6
+    assert y2 == 9
